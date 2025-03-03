22ab609a2d2d668edd8a889c18b34d9f100b7484
karunanidhi <karunanidhiayyamperumal@gmail.com>
2025-03-04 01:25:01 +0530

    added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,20 +4,22 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import Dashboard from "./pages/Dashboard"; // New Page
 import LoginPage from "./pages/LoginPage"; // Example Login Page
+import PeakHour from "./pages/peakHour";
 
 function App() {
   return (
-    <Router>
+  
       <div className="min-h-screen bg-gray-100">
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/station/dashboard" element={<Dashboard />} />
+          <Route path="/station/dashboard/peakhour" element={<PeakHour />} />
           <Route path="/grid/dashboard" element={<Dashboard />} />
           <Route path="/login" element={<LoginPage />} />
         </Routes>
       </div>
-    </Router>
+    
   );
 }
 
